0c2f01f94bf98164c335cba6bf9b429ecf89368e
Eli Foster <elifosterwy@gmail.com>
2015-08-31 23:54:55 -0700

    Add forgotten ifs in CurseForge upload

--- src/utils.rb
+++ src/utils.rb
@@ -11,10 +11,18 @@ module Bot
       request = URI(@cfapi)
       request.query = URI.encode_www_form(params)
       response = Net::HTTP.get_response(request)
-      if response.is_a? Net::HTTPSuccess
+      if response.is_a? Net::HTTPSuccess || response == '201'
         return true
       else
-        if response.
+        if response == '403'
+          exit "You do not have permission to edit that project, or your API key was incorrect."
+        elsif response == '404'
+          exit "Project could not be found"
+        elsif response == '405'
+          exit "ModUpdater dev did GET instead of POST. Please report this to the author."
+        elsif response == '422'
+          exit "Provided parameters have an error in their form. Please check your JSON or report it to the ModUpdater author"
+        end
       end
     end
   end
